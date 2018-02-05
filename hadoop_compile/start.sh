b12713f0147c4a3320e97528fef21466778750d3
Sina Kashipazha <esterlinkof@gmail.com>
2018-02-06 00:19:16 +0330

    Ready to test on containernet.

--- hadoop_compile/start.sh
+++ hadoop_compile/start.sh
@@ -5,6 +5,10 @@ HADOOP_HOSTS="${HADOOP_HOSTS:-127.0.0.1 master}"
 echo -e "\n#Hadoop hosts" >> /etc/hosts
 echo "$HADOOP_HOSTS" | sed -e $'s/,/\\\n/g' >> /etc/hosts
 
+echo "$HADOOP_HOSTS" | \
+    sed -e $'s/,/\\\n/g' | \
+    awk '{print $2}' > $HADOOP_HOME/etc/hadoop/slaves
+
 # set my role
 MY_ROLE="${MY_ROLE:-slave}"
 
@@ -26,8 +30,15 @@ echo ${HOST_NUMBERS}
 echo ${IS_SINGLE}
 echo ${MY_ROLE}
 
-service ssh restart && \
-    $HADOOP_HOME/sbin/start-dfs.sh && \
-    $HADOOP_HOME/sbin/start-yarn.sh && \
-    hdfs dfs -mkdir -p /user/sina/data && \
-    hdfs dfs -copyFromLocal /home/mahdiz.big /user/sina/data
+if [ "${IS_SINGLE}" == false ] && [ "${MY_ROLE}" == "master" ]; then
+    echo "######################## hello #####################"
+    echo "$HADOOP_HOSTS" | \
+        sed -e $'s/,/\\\n/g' | \
+        awk '{print $2}' | \
+        while read line ; do ssh-keyscan ${line} >> ~/.ssh/known_hosts ; done
+fi
+
+#$HADOOP_HOME/sbin/start-dfs.sh && \
+#    $HADOOP_HOME/sbin/start-yarn.sh && \
+#    hdfs dfs -mkdir -p /user/sina/data && \
+#    hdfs dfs -copyFromLocal /home/mahdiz.big /user/sina/data
